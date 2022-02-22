c201dea7f91ab866af769675cf1daf3a1e8fba6b
Connor-DeMarco <demarco.cmj@gmail.com>
2022-02-22 15:56:46 -0800

    gun swapping sync added

--- Squares Game/Assets/Scripts/Player Movement/PlayerController.cs
+++ Squares Game/Assets/Scripts/Player Movement/PlayerController.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks
 {
     [SerializeField] float mouseSens, sprintSpeed, walkSpeed, jumpForce, smoothTime;
     [SerializeField] GameObject camHolder;
+    [SerializeField] Item[] items;
 
+    int itemIndex;
+    int prevItemIndex = -1;
 
     float verticalLookRotation;
     bool isGrounded;
@@ -25,7 +30,11 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        if(!PV.IsMine)
+        if(PV.IsMine)
+        {
+            EquipItem(0);
+        }
+        else
         {
             Destroy(GetComponentInChildren<Camera>().gameObject);
             Destroy(rb); //Helps to smooth movement sync, Destroys RB of other players. NOTE: physics object bug in future?
@@ -40,8 +49,46 @@ public class PlayerController : MonoBehaviour
         MouseLook();
         Move();
         Jump();
+
+
+        //Gun Item Swapping
+        for(int i = 0; i < items.Length; i++)
+        {
+            if(Input.GetKeyDown((i + 1).ToString()))
+            {
+                EquipItem(i);
+                break;
+            }
+        }
+        
+        if(Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+        {
+            if(itemIndex >= items.Length - 1)
+            {
+                EquipItem(0);
+            }
+            else
+            {
+                EquipItem(itemIndex + 1);
+            }
+
+        }
+        else if(Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+        {
+            if(itemIndex <= 0)
+            {
+                EquipItem(items.Length - 1);
+            }
+            else
+            {
+                EquipItem(itemIndex - 1);
+            }
+
+        }
     }
 
+
+    /************************* MOVEMENT *************************/
     void MouseLook()
     {
         //Mouse Look transforms
@@ -80,4 +127,37 @@ public class PlayerController : MonoBehaviour
         rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
     }
 
+     /************************* ITEMS *************************/
+
+     void EquipItem(int tempIndex)
+     {
+         if(tempIndex == prevItemIndex)
+            return;
+
+         itemIndex = tempIndex;
+         items[itemIndex].itemObj.SetActive(true);
+
+         if(prevItemIndex != -1)
+         {
+             items[prevItemIndex].itemObj.SetActive(false);
+         }
+
+         prevItemIndex = itemIndex;
+
+        //Sync With other players, E7@3:00
+        if(PV.IsMine)
+        {
+            Hashtable hash = new Hashtable();
+            hash.Add("itemIndex", itemIndex);
+            PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+        }
+     }
+
+	public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+	{
+		if(!PV.IsMine && targetPlayer == PV.Owner)
+		{
+			EquipItem((int)changedProps["itemIndex"]);
+		}
+	}
 }
